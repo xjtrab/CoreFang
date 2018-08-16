fc04f2f580ed7a4872ec4ca651c002f90ba88f37
Xiaojian.Tang <Xiaojian.Tang@partner.bmw.com>
2018-08-16 10:14:29 +0800

    list done

--- CoreCfang/Program.cs
+++ CoreCfang/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 using HtmlAgilityPack.CssSelectors.NetCore;
 
@@ -20,9 +21,12 @@ namespace CoreCfang
             foreach (var item in test)
             {
                 var price = item.QuerySelector(".area").QuerySelector(".gray").InnerText.Replace(Environment.NewLine, "").Trim();
+                var totalPrice = Regex.Replace(item.QuerySelector(".area").QuerySelector(".orange").InnerText.Replace(Environment.NewLine, "").Trim(),@"\s","");
+                var mianji = Regex.Replace(item.QuerySelector(".horizontal").QuerySelector(".horizon-detail").InnerText.Replace(Environment.NewLine, "").Trim(),@"\s","").Replace("&nbsp;&nbsp;"," ");
+
                 var href = item.QuerySelector("a").Attributes["href"];
                 var id  = href.Value.Split("/")[3];
-                Console.WriteLine("Price : " + price);
+                Console.WriteLine("总价 : " + totalPrice + " 面积:" +mianji + " 单价:" + price + " id:" + id );
             }
         }
     }
